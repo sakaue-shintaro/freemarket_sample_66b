91b7f1e6c167425b76871a13eeaff81331c4e08b
onikunai <greentea5461@gmail.com>
2020-01-09 20:45:26 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,11 @@ ActiveRecord::Schema.define(version: 20200109072815) do
   end
 
   create_table "signups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "body",       limit: 65535
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_signups_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -34,4 +37,5 @@ ActiveRecord::Schema.define(version: 20200109072815) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "signups", "users"
 end
