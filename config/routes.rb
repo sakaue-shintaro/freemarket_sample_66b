e6282da0271cfd176ca26dada4207f2729c06a04
sakaue-shintaro <blues_feeling_0184_1115@yahoo.co.jp>
2020-01-09 18:15:10 +0900

    front-start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  get 'signup/index'  => 'signup#index'    
+  get 'signup/index'  => 'signup#index'
+  get 'mypages/index'  => 'mypages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
