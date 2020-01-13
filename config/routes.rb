0d2f4045bc5f3f0aa0fd8041490ec90603ffbfe7
onikunai <greentea5461@gmail.com>
2020-01-13 12:56:47 +0900

    mixin

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   get 'signup/index'  => 'signup#index'
   get 'mypages/index'  => 'mypages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  # 下記の囲いは、作業用仮設定。あとで、必ず削除する（かも）
+  # -----------------------------------------------------------------------------
+  get 'signup/registration_data1'  => 'signup#ragistration_data1'
+  get 'signup/registration_data2'  => 'signup#ragistration_data2'
+  get 'signup/registration_data3'  => 'signup#ragistration_data3'
+  get 'signup/registration_data4'  => 'signup#ragistration_data4'
+  get 'signup/registration_data5'  => 'signup#ragistration_data5'
+  # -----------------------------------------------------------------------------
 end
