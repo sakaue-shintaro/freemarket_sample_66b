ec70382752866681b415fb50ccba87371d137118
katosyogo <seiten.hkss321@gmail.com>
2020-01-16 12:14:14 +0900

    test_mysql

---
+++ app/assets/javascripts/mypage.js
@@ -0,0 +1,17 @@
+$(function() {
+  //クリックしたときのファンクションをまとめて指定
+  $('.tab li').click(function() {
+  //.index()を使いクリックされたタブが何番目かを調べ、
+  //indexという変数に代入します。
+        var index = $('.tab li').index(this);
+    //コンテンツを一度すべて非表示にし、
+      $('.contents li').css('display','none');
+    //クリックされたタブと同じ順番のコンテンツを表示します。
+    $('.contents li').eq(index).css('display','block');
+    //一度タブについているクラスselectを消し、
+    $('.tab li').removeClass('select');
+    //クリックされたタブのみにクラスselectをつけます。
+    $(this).addClass('select')
+  });
+});
+  
