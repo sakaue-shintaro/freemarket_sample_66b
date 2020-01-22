708640d3746617a863d84ef3023cc87efd329abf
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2020-01-22 17:17:49 +0900

    products_jquery
    画像の複数投稿
    手数料の自動計算

--- app/assets/javascripts/products.js
+++
@@ -1,6 +0,0 @@
-$(document).on ('turbolink:load', ()=> {
-  // 画像用のinputを生成する関数
-  const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">` 
-  }
-})
---
+++ app/assets/javascripts/products/products.js
@@ -0,0 +1,78 @@
+$(function() {
+  // 画像用のinputを生成する関数
+  const buildFileField = (num)=> {
+    const html = `<div data-index="${num}" class="js-file_group">
+                    <input class="js-file" type="file"
+                    name="product[images_attributes][${num}][src]"
+                    id="product_images_attributes_${num}_src" style="display:none"><label for="product_images_attributes_${num}_src"><pre>クリックしてファイルをアップロード</pre></label>
+                  </div>`;
+    return html;
+  }
+  // プレビュー用のimgタグを生成する関数
+  const buildImg = (index, url)=> {
+    const html = `<div class="kote">
+                    <img data-index="${index}" src="${url}" width="100px" height="100px">
+                    <div class="imgedit">
+                      <div class="js-remove">編集</div>
+                      <div class="js-remove">削除</div>
+                    </div>
+                  </div>
+                  `;
+    return html;
+  }
+
+  // file_fieldのnameに動的なindexをつける為の配列
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  // 既に使われているindexを除外
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+
+  $('.hidden-destroy').hide();
+
+  $('#image-box').on('change', '.js-file', function(e) {
+    const targetIndex = $(this).parent().data('index');
+    // ファイルのブラウザ上でのURLを取得する
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+
+    // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('src', blobUrl);
+    } else {  // 新規画像追加の処理
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+      // fileIndexの先頭の数字を使ってinputを作る
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      // 末尾の数に1足した数を追加する
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    }
+  });
+
+  $('#image-box').on('change', '.js-file', function() {
+    const targetIndex = $(this).parent().data('index');
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+
+    $(this).parent().after($(this).clone()).remove();
+    // $(`img[data-index="${targetIndex}"]`).remove();
+
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  });
+})
+
+function update_field(){
+  let value = 0.9;
+  let comis = 0.1;
+  let result = $('#product_price').val() * value;
+  $('#profit').text("¥"+result);
+  let pami = $('#product_price').val() * comis;
+  $('#commission').text("¥"+pami);
+}
+$(function() {
+  $('input[type="text"]').on('keyup change', function() {
+    update_field();
+  });
+});
