36cf9c3df9ae739c4137e0b86acaef887f27d0bb
onikunai <greentea5461@gmail.com>
2020-01-23 18:14:11 +0900

    test
    これは、無視する。

--- app/models/address.rb
+++
@@ -1,8 +0,0 @@
-class Address < ApplicationRecord
-  belongs_to :user, optional: true
-  
-  validates :postal_code, presence: true
-  validates :prefectuer, presence: true
-  validates :city_ward, presence: true
-  validates :address, presence: true
-end
