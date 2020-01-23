c2535b323c5cca51b784516c30f234ee6918af83
onikunai <greentea5461@gmail.com>
2020-01-23 20:10:53 +0900

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products
-
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'step5'
+    end
+  end
 
 
   
