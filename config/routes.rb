43a2ee4b1c552504851f98218a1b452cca681c83
onikunai <greentea5461@gmail.com>
2020-01-23 20:06:17 +0900

    test_user_singup

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products
-  resources :signup do
-    collection do
-      get 'step1'
-      get 'step2'
-      get 'step3'
-      get 'step4'
-      get 'step5'
-    end
-  end
+
 
 
   
