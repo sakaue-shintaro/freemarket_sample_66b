b33eaabcfd49d32b80f4b220e4b7e713862ec3bd
katosyogo <seiten.hkss321@gmail.com>
2020-01-24 19:06:20 +0900

    test3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
 
   factory :user do
     password = Faker::Internet.password(8)
-    email   {Faker::Internet.unique.email}
+    email = Faker::Internet.unique.email
     nickname                  {"あーべ"}
     family_name               {"abe"}
     first_name                {"takashi"}
     j_family_name             {"アベ"}
     j_first_name              {"タカシ"}
-    # email                     { email }
+    email                     { email }
     phonennumber              {"00000000"}
     birthday_year             {"00000000"}
     birthday_month            {"00000000"}
@@ -18,3 +18,25 @@ FactoryBot.define do
   end
 
 end
+
+
+
+
+# FactoryBot.define do
+#   factory :user do
+#     password = Faker::Internet.password(8)
+#     email   {Faker::Internet.unique.email}
+#     nickname                  {“あーべ“}
+#     family_name               {“abe”}
+#     first_name                {“takashi”}
+#     j_family_name             {“アベ“}
+#     j_first_name              {“タカシ“}
+#     # email                     { email }
+#     phonennumber              {“00000000”}
+#     birthday_year             {“00000000"}
+#     birthday_month            {“00000000”}
+#     birthday_day              {“00000000"}
+#     password                  { password }
+#     password_confirmation     { password }
+#   end
+# end
