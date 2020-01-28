b9d2b581c2914af575ea73f54a47d8cb7781badc
sakaue-shintaro <blues_feeling_0184_1115@yahoo.co.jp>
2020-01-28 19:04:06 +0900

    pey-key

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,7 +9,7 @@ class PurchasesController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = 'sk_test_88ede2748be3db6794ece94e'
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -20,7 +20,7 @@ class PurchasesController < ApplicationController
   def pay
     card = Card.where(user_id: current_user.id).first
     @product = Product.find(params[:id])
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp.api_key = 'sk_test_88ede2748be3db6794ece94e'
     Payjp::Charge.create(
     :amount => @product.price #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
