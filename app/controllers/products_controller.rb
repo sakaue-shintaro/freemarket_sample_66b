c335f506a83c2d11b936951df772f85f7992f6c6
sakaue-shintaro <blues_feeling_0184_1115@yahoo.co.jp>
2020-01-29 14:22:17 +0900

    com

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,9 @@ class ProductsController < ApplicationController
     end
   end
 
-
+  def edit
+    @product = Product.find(params[:id])
+  end 
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
