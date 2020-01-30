58ad0750097b546a67b45939f21e34147502e250
sakaue-shintaro <blues_feeling_0184_1115@yahoo.co.jp>
2020-01-30 11:40:09 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'team-b' && password == '1qaz2wsx'
+    end
+  end
 
   
 end
