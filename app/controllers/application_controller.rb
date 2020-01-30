41fde11212fb6b28e6f24997b6100b2023e418a1
sakaue-shintaro <blues_feeling_0184_1115@yahoo.co.jp>
2020-01-30 12:17:49 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
